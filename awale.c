baae13eb85e31617c27085a996d1870d4bab0bfc
Meije <meije.pigeonnat@insa-lyon.fr>
2023-11-27 17:56:00 +0100

    Ajout cas 3 checkleglMove

--- awale.c
+++ awale.c
@@ -2,50 +2,43 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "awale.h"
+struct player {
+  int playerNumber;
+  char* playerName;
+  int score;
+};
 
+struct awale {
+  struct player* player1;
+  struct player* player2;
+  int currentPlayer;
+  int endOfGame;
+  int gameState[12];
+};
 
-Awale initGame(char* player1Name, char* player2Name) {
+struct player* player1;
+struct player* player2;
+int currentPlayer = 1;
+int endOfGame = false;
+int gameState[12];
 
-  Player *player1 = (Player *)malloc(sizeof(Player));
+void initGame(char* player1Name, char* player2Name) {
+  player1 = (struct player*)malloc(sizeof(struct player));
   player1->playerName = player1Name;
   player1->playerNumber = 1;
   player1->score = 0;
 
-  Player *player2 = (Player *)malloc(sizeof(Player));
+  player2 = (struct player*)malloc(sizeof(struct player));
   player2->playerName = player2Name;
   player2->playerNumber = 2;
   player2->score = 0;
 
-  int * gameState = (int *)malloc(sizeof(int)*12);
   for(int i = 0; i < 12; i++) {
     gameState[i] = 3;
   }
-  Awale game = { player1, player2, 1, false, gameState };
-  return game;
-
 }
 
-/*void printGameState() {
-  printf("\n\n  11 10  9  8  7  6\n");
-  printf("---------------------\n| ");
-  for(int i = 11; i > 5; i--) {
-    gameState[i] < 10 ?
-    printf(" %d ", gameState[i]) :
-    printf("%d ", gameState[i]);
-  }
-  printf("|       Score\n| ");
-  for(int i = 0; i <6; i++) {
-    gameState[i] < 10 ?
-    printf(" %d ", gameState[i]) :
-    printf("%d ", gameState[i]);
-  }
-  printf("|       Player 1: %d - Player 2: %d\n---------------------\n   0  1  2  3  4  5\n\n", player1->score, player2->score);
-  printf("Player %d to play\n", currentPlayer);
-}*/
-
-void printGameState() {
-  char * gameBoard;
+void printGameState(char * gameBoard) {
   strcat(gameBoard, "\n\n  11 10  9  8  7  6\n");
   strcat(gameBoard, "---------------------\n| ");
 
@@ -78,16 +71,37 @@ void printGameState() {
   snprintf(temp, sizeof(temp), " %d ", player2->score);
   strcat(gameBoard, temp);
   strcat(gameBoard, "\n---------------------\n   0  1  2  3  4  5\n\n");
-  printf(gameBoard);
-  printf("Player %d to play\n", currentPlayer);
 }
 
 int checkLegalMove(int playerNumber, int tile) {
+  //Cas  1 : pas de graine  dans l'endroit selecctionnz
   if(gameState[tile] == 0) return false;
+
+  //Cas 2 : numero selectionne hors zone de jeu
   if(playerNumber == 1 && (tile < 0 || tile > 5)) return false;
   if(playerNumber == 2 && (tile < 6 || tile > 11)) return false;
-  //Dernier cas: si plus de bille chez adversaire obligé de donner bille
-  //Pas prendre en un seul coup toutes les graines de leur adversaire #BLC
+
+  //Cas 3: si plus de bille chez adversaire obligé de donner bille
+  int opponentPlayer = (playerNumber==1) ? 2 : 1;
+  int currentSide = (playerNumber==1) ? 0 : 6;
+  int opponentSide = (opponentPlayer==1) ? 0 : 6;
+  int sumOpponentSeeds = 0;
+  for (int i = opponentSide; i < opponentSide + 6; i++){
+    sumOpponentSeeds += gameState[i];
+  }
+  int possible = 0;
+  if(sumOpponentSeeds == 0){
+    for(int i = currentSide; i< currentSide+6; i++){
+      if(gameState[i] > (6-i-currentSide)) {
+        possible = 1;
+        break;
+      }
+    }
+    if(possible == 1 && gameState[tile] <= (6-tile-currentSide)){
+      return false;
+    }
+  }
+  //Cas 4 : Pas prendre en un seul coup toutes les graines de leur adversaire #BLC
   return true;
 }
 
@@ -135,9 +149,11 @@ int playTurn(int tile){
 int main() {
   initGame("test", "me");
   printf("Bienvenue dans cette nouvelle partie Joueur 1 vous commencez !!\n");
+  char* gameBoard = malloc(4096);
 
   while (endOfGame == false || endOfGame == ERR_TILE_NUMBER) {
-    printGameState();
+    printGameState(gameBoard);
+    printf("%s",  gameBoard);
     int tile = 0;
     scanf("%d", &tile);
     endOfGame = playTurn(tile);
@@ -145,4 +161,5 @@ int main() {
   printf("Bravo ! Le joueur %d a gagné :-)\n", currentPlayer);
   free(player1); 
   free(player2);
+  free(gameBoard);
 }
